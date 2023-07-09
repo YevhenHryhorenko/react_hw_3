bb65df8219c9b80f7121bb5c318ace30d317a205
YevhenHryhorenko <116802765+YevhenHryhorenko@users.noreply.github.com>
2023-07-09 21:07:22 +0300

    add todo list app

---
+++ src/main.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.jsx'
+import './index.css'
+
+const DATA = [
+  { id: "todo-0", name: "Eat", completed: false },
+  { id: "todo-1", name: "Sleep", completed: false },
+  { id: "todo-2", name: "Repeat", completed: false },
+];
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <App tasks={DATA} />
+)
